d854842f20d7cdd774dbe83c0c3bf0fbe17bf3bb
Puvvada <puvvada.sagar.learnings@gmail.com>
2022-11-25 21:26:31 +0530

    Added new test method

--- Test.java
+++ Test.java
@@ -2,4 +2,7 @@ class Test{
     public static void main(String args[]){
         System.out.println("Test method")
     }
+    public void testJava(){
+        System.out.println("Hey i'm inside Test java")
+    }
 }
